7986ad5903b6904061e50a5415ffff3eae1f8e03
drew <58788781+drew138@users.noreply.github.com>
2024-04-27 09:58:36 -0500

    feat: wip

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
 export default {
-    endpoint: 'http://localhost:8000/api/checkout',
+    endpoint: 'http://34.29.109.190:8003',
     stripe_key: 'pk_test_51P4tXiIdhZFpJSS6P4i4yKBH5NrBA35lcEUWk1CzGoOgvymczzPAf2Ocs50Q0WiBcxrYaB2fa7B5xaosIjTa1QkZ00Aw4UE37j'
 }
